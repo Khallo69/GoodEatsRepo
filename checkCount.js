59ead23fa7b9e1ff32d456b9c8f5c4e239bbe475
Khallo69 <46410798+Khallo69@users.noreply.github.com>
2019-02-18 13:17:32 -0500

    Add files via upload

--- checkCount.js
+++ checkCount.js
@@ -1,71 +1,87 @@
 
 function countChecks(checkArray, checkBoxNum, foodCheckCount){
-		var count = 0;
-		totalChecks = 15;
-		randomArray = [1,2,3];
+	var count = 0;
+	totalChecks = 15;
+	randomArray = [1, 2, 3, 4, 5];
 
-		if(checkBoxNum == checkArray[4])
-			count = 1;
-		else if(checkBoxNum == checkArray[3])
-			count = 2;
-		else if(checkBoxNum == checkArray[2])
-			count = 3;
-		else if(checkBoxNum == checkArray[1])
-			count = 4;
-		else
-			count = 5;
-		if(checkBoxNum.checked == true)
-		{	
-			for(let i = 0; i < checkArray.length - count; i++)
-			{
-				checkArray[i].checked = true;
-			}
-		}
-		else if(checkBoxNum.checked == false)
+	if(checkBoxNum == checkArray[4])
+		count = 1;
+	else if(checkBoxNum == checkArray[3])
+		count = 2;
+	else if(checkBoxNum == checkArray[2])
+		count = 3;
+	else if(checkBoxNum == checkArray[1])
+		count = 4;
+	else
+		count = 5;
+	if(checkBoxNum.checked == true)
+	{	
+		for(let i = 0; i < checkArray.length - count; i++)
 		{
-			for(let i = 4; i > checkArray.length - count; i--)
-			{
-				checkArray[i].checked = false;
-			}
+			checkArray[i].checked = true;
 		}
-		for(let i = 0; i < checkArray.length; i++)
-		{		
-			if(checkArray[i].checked == true){
-				foodCheckCount += 1;
-			}
-		}
-		if(checkArray == tacoCheckArray)
-		{
-			totalTacoChecks = foodCheckCount;	
-		}
-		if(checkArray == panCheckArray)
-		{
-			totalPanChecks = foodCheckCount;
-		}
-		if(checkArray == chipCheckArray)
-		{
-			totalChipChecks = foodCheckCount;		
-		}
-		totalChecks -= (totalTacoChecks + totalPanChecks + totalChipChecks);
-		document.getElementById("checkNum").innerHTML = "Points Left: " + totalChecks;
-		if(totalChecks < 15)
-		{
-			randomArray = [];
-		}
-		else
-		{
-			randomArray = [1, 2, 3];
-		}
-		for(var i = 0; i < totalTacoChecks; i++)
-			{
-				randomArray.push(1);
-			}
-		for(var i = 0; i < totalPanChecks; i++)
-			{
-				randomArray.push(2);
-			}
-		for(var i = 0; i < totalChipChecks; i++)
-			{
-				randomArray.push(3);			
-			}
 	}
+	else if(checkBoxNum.checked == false)
+	{
+		for(let i = 4; i > checkArray.length - count; i--)
+		{
+			checkArray[i].checked = false;
+		}
+	}
+	for(let i = 0; i < checkArray.length; i++)
+	{		
+		if(checkArray[i].checked == true){
+			foodCheckCount += 1;
+		}
+	}
+	if(checkArray == tacoCheckArray)
+	{
+		totalTacoChecks = foodCheckCount;	
+	}
+	if(checkArray == panCheckArray)
+	{
+		totalPanChecks = foodCheckCount;
+	}
+	if(checkArray == chipCheckArray)
+	{
+		totalChipChecks = foodCheckCount;		
+	}
+	if(checkArray == macCheckArray)
+	{
+		totalMacChecks = foodCheckCount;
+	}
+	if(checkArray == wendCheckArray)
+	{
+		totalWendChecks = foodCheckCount;
+	}
+	totalChecks -= (totalTacoChecks + totalPanChecks + totalChipChecks + totalMacChecks + totalWendChecks);
+	document.getElementById("checkNum").innerHTML = "Points Left: " + totalChecks;
+	if(totalChecks < 15)
+	{
+		randomArray = [];
+	}
+	else
+	{
+		randomArray = [1, 2, 3, 4, 5];
+	}
+	for(var i = 0; i < totalTacoChecks; i++)
+		{
+			randomArray.push(1);
+		}
+	for(var i = 0; i < totalPanChecks; i++)
+		{
+			randomArray.push(2);
+		}
+	for(var i = 0; i < totalChipChecks; i++)
+		{
+			randomArray.push(3);			
+		}
+	for(var i = 0; i < totalMacChecks; i++)
+		{
+			randomArray.push(4);			
+		}
+	for(var i = 0; i < totalWendChecks; i++)
+		{
+			randomArray.push(5);			
+		}
+}
--- randImg.js
+++ randImg.js
@@ -1,38 +1,45 @@
 function randomImage(val){
-		var image = document.getElementById('myImage');
-		var elem = document.getElementById("myBar");   
-  		var width = 1;
-		var id = setInterval(frame, 10);
-		image.src = "https://upload.wikimedia.org/wikipedia/commons/5/59/Empty.png";
-		function frame() {
-			if (width >= 100) {
-				clearInterval(id);
-				rollButton.disabled = false;
-				rollButton.style.backgroundColor = "#09bc38";
-				switch(val) {
-					case 1:
-						image.src = "https://cdn.freebiesupply.com/logos/large/2x/taco-bell-5-logo-png-transparent.png";
-						document.getElementById("foodHeader").innerHTML = "Tight!";
-						break;
+console.log(randomArray);
+	var image = document.getElementById('myImage');
+	var elem = document.getElementById("myBar");   
+	var width = 1;
+	var id = setInterval(frame, 10);
+	image.src = "https://upload.wikimedia.org/wikipedia/commons/5/59/Empty.png";
+	function frame() {
+		if (width >= 100) {
+			clearInterval(id);
+			rollButton.disabled = false;
+			rollButton.style.backgroundColor = "#09bc38";
+			switch(val) {
+				case 1:
+					image.src = "https://cdn.freebiesupply.com/logos/large/2x/taco-bell-5-logo-png-transparent.png";
+					document.getElementById("foodHeader").innerHTML = "Tight!";
+					break;
 
-					case 2:
-						image.src = "https://cdn.freebiesupply.com/logos/large/2x/panera-bread-logo-png-transparent.png";
-						document.getElementById("foodHeader").innerHTML = "Light!";
-						break;
+				case 2:
+					image.src = "https://cdn.freebiesupply.com/logos/large/2x/panera-bread-logo-png-transparent.png";
+					document.getElementById("foodHeader").innerHTML = "Light!";
+					break;
 
-					case 3:
-						image.src = "https://cdn.freebiesupply.com/logos/large/2x/chipotle-mexican-grill-logo-png-transparent.png";
-						document.getElementById("foodHeader").innerHTML = "Right!";
-						break;
-				}
-			} else {
-				width++; 
-				elem.style.width = width + '%'; 
-				rollButton.disabled = true;
-				rollButton.style.backgroundColor = "#277730";
-				document.getElementById("foodHeader").innerHTML = "Rolling!";
+				case 3:
+					image.src = "https://cdn.freebiesupply.com/logos/large/2x/chipotle-mexican-grill-logo-png-transparent.png";
+					document.getElementById("foodHeader").innerHTML = "Right!";
+					break;
+				case 4:
+					image.src = "https://seeklogo.net/wp-content/uploads/2011/04/old-mcdonalds-vector-logo.png";
+					document.getElementById("foodHeader").innerHTML = "Might!";
+					break;
+				case 5:
+					image.src = "http://www.stickpng.com/assets/images/58429d58a6515b1e0ad75ae8.png";
+					document.getElementById("foodHeader").innerHTML = "Wight!";
+					break;
 			}
-  		}
-
-		
-	}
+		} else {
+			width++; 
+			elem.style.width = width + '%'; 
+			rollButton.disabled = true;
+			rollButton.style.backgroundColor = "#277730";
+			document.getElementById("foodHeader").innerHTML = "Rolling!";
+		}
+		}
+}
